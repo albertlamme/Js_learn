925cf70324807abad78b5ff116bc9abd97efb291
ala20244 <albertlamme@gmail.cmo>
2017-05-16 14:11:06 +0200

    Changes

--- script.js
+++ script.js
@@ -1,106 +1,106 @@
-// // function outOfBed (){
-// //     var photoscenarios = confirm('Wekker gaat om 6:00. Ga ik uit bed om te fotograferen');
-// //     if (photoscenarios == true) {
-// //         alert('Hup uit bed');
-// //         takePicture()
-// //     } else {
-// //         alert('Lekker blijven liggen')
-// //     }
-// // }
-// //
-// // function takePicture() {
-// //     var pictureScenario = prompt("Bij de natuurpark aangekomen zie je dat de zon al snel op komt. Wat ga je doen?").toLowerCase();
-// //     if(pictureScenario == 'tripod') {
-// //         alert('Je neemt de tijd en denkt beter 1 kwalitatieve foto dan meerdere slechte fotos');
-// //     } else if (pictureScenario == 'hand') {
-// //         alert('Veel fotos maken, dat is wat je wil');
-// //         takePhotos();
-// //     } else {
-// //         alert('Je hebt geen zin om te haasten en laat fotografie voor wat het is.')
-// //     }
-// // }
-// //
-// // function takePhotos(){
-// //     var photos = ["photo1","photo2","photo3"];
-// //     var shutter = confirm('Neem een foto');
-// //     if (shutter == true){
-// //         photos.push("photo4");
-// //         for(i=0;i < photos.length; i++) {
-// //             console.log(photos[i]);
-// //         }
-// //
-// //     } else {
-// //         alert("geen foto genomen")
-// //     }
-// // }
-//
-// // TODO
-// // - data atributen
-// // - image upload
-// // - object geporienteerd
-//
-//
-//
-// 'use sctrict'
-//
-// var pics = [""];
-// var container = document.getElementById("gallery");
-//
-//
-// function createGallery() {
-//     for (i = 0; i < pics.length; i++){
-//         var imageContainer = document.createElement("div");
-//         var imageElement = document.createElement("img");
-//         imageElement.setAttribute("src",pics[i]);
-//         imageElement.setAttribute("class",'imgContainer');
-//         imageContainer.setAttribute("onclick","deleteImage(this)");
-//         imageContainer.classList.add("imageContainer");
-//         container.appendChild(imageContainer);
-//         imageContainer.appendChild(imageElement);
+// function outOfBed (){
+//     var photoscenarios = confirm('Wekker gaat om 6:00. Ga ik uit bed om te fotograferen');
+//     if (photoscenarios == true) {
+//         alert('Hup uit bed');
+//         takePicture()
+//     } else {
+//         alert('Lekker blijven liggen')
 //     }
 // }
 //
-// // function imgAcrtions(){
-// //
-// //     var images = document.getElementsByClassName('imgContainer');
-// //     images.onclick = function(){
-// //         deleteImage(this);
-// //     }
-// //
-// // }
-//
-// function addImage() {
-//     deleteAll();
-//     pics.push("images/10-03-2017-4.jpg");
-//     createGallery();
-// }
-//
-// function deleteAll(){
-//     var images = document.getElementsByTagName('img');
-//     while(images.length > 0) {
-//         images[0].parentNode.removeChild(images[0]);
+// function takePicture() {
+//     var pictureScenario = prompt("Bij de natuurpark aangekomen zie je dat de zon al snel op komt. Wat ga je doen?").toLowerCase();
+//     if(pictureScenario == 'tripod') {
+//         alert('Je neemt de tijd en denkt beter 1 kwalitatieve foto dan meerdere slechte fotos');
+//     } else if (pictureScenario == 'hand') {
+//         alert('Veel fotos maken, dat is wat je wil');
+//         takePhotos();
+//     } else {
+//         alert('Je hebt geen zin om te haasten en laat fotografie voor wat het is.')
 //     }
 // }
 //
-// function deleteFirst(){
-//     deleteAll();
-//     pics.splice(0,1);
-//     createGallery();
+// function takePhotos(){
+//     var photos = ["photo1","photo2","photo3"];
+//     var shutter = confirm('Neem een foto');
+//     if (shutter == true){
+//         photos.push("photo4");
+//         for(i=0;i < photos.length; i++) {
+//             console.log(photos[i]);
+//         }
+//
+//     } else {
+//         alert("geen foto genomen")
+//     }
 // }
+
+// TODO
+// - data atributen
+// - image upload
+// - object geoorienteerd
+
+
+
+'use sctrict'
+
+var pics = ["images/10-03-2017-1.jpg","images/10-03-2017-2.jpg","images/10-03-2017-3.jpg"];
+var container = document.getElementById("gallery");
+
+
+function createGallery() {
+    for (i = 0; i < pics.length; i++){
+        var imageContainer = document.createElement("div");
+        var imageElement = document.createElement("img");
+        imageElement.setAttribute("src",pics[i]);
+        imageElement.setAttribute("class",'imgContainer');
+        imageContainer.setAttribute("onclick","deleteImage(this)");
+        imageContainer.classList.add("imageContainer");
+        container.appendChild(imageContainer);
+        imageContainer.appendChild(imageElement);
+    }
+}
+
+// function imgAcrtions(){
 //
-// function deleteLast(){
-//     deleteAll();
-//     pics.pop();
-//     createGallery();
+//     var images = document.getElementsByClassName('imgContainer');
+//     images.onclick = function(){
+//         deleteImage(this);
+//     }
+//
 // }
-//
-// function deleteImage(image) {
-//     container.removeChild(image);
-// }
-//
-//
-//
-//
+
+function addImage() {
+    deleteAll();
+    pics.push("images/10-03-2017-4.jpg");
+    createGallery();
+}
+
+function deleteAll(){
+    var images = document.getElementsByTagName('img');
+    while(images.length > 0) {
+        images[0].parentNode.removeChild(images[0]);
+    }
+}
+
+function deleteFirst(){
+    deleteAll();
+    pics.splice(0,1);
+    createGallery();
+}
+
+function deleteLast(){
+    deleteAll();
+    pics.pop();
+    createGallery();
+}
+
+function deleteImage(image) {
+    container.removeChild(image);
+}
+
+
+
+
 // function imageSelected(){
 //
 //     var file = document.getElementById("file-select").files[0];
@@ -116,58 +116,39 @@
 //         document.getElementById("fileType").innerHTML = 'Type:' + file.type;
 //     }
 // }
-//
-// function UploadImage(){
-//     var fd = new FormData();
-//     fd.append("image", document.getElementById("file-select").files[0]);
-//     var xhr = new XMLHttpRequest();
-//     xhr.upload.addEventListener("progress", uploadProgress, false);
-//     xhr.addEventListener("load", uploadComplete, false);
-//     xhr.addEventListener("error", uploadError, false);
-//     xhr.addEventListener("abort", uploadAbort, false);
-//     xhr.open("POST", "handler.php");
-//     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-//     xhr.send(fd);
-// }
-//
-// function uploadProgress(e) {
-//     if (e.lengthComputable) {
-//         var percentComplete = Math.round(e.loaded * 100 / e.total);
-//         document.getElementById("progress").innerHTML = percentComplete.toString() + '%';
-//     } else {
-//         document.getElementById("progress").innerHTML = "unable to compute";
-//     }
-// }
-//
-// function uploadComplete (e) {
-//     alert(e.target.responseText);
-// }
-//
-// function uploadError (e) {
-//     alert("Error");
-// }
-//
-// function uploadAbort (e) {
-//     alert("Abort");
-// }
 
-function jsonAjax(){
-    var hr = new XMLHttpRequest();
-    var results = document.getElementById("gallery");
+function UploadImage(){
+    var fd = new FormData();
+    fd.append("image", document.getElementById("file-select").files[0]);
+    console.log(fd);
+    var xhr = new XMLHttpRequest();
+    xhr.upload.addEventListener("progress", uploadProgress, false);
+    xhr.addEventListener("load", uploadComplete, false);
+    xhr.addEventListener("error", uploadError, false);
+    xhr.addEventListener("abort", uploadAbort, false);
+    xhr.open("POST", "handler.php");
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(fd);
 
-    hr.open("GET","my_images.php",true);
-    hr.setRequestHeader("Content-type", "application/json", true);
-    hr.onreadystatechange = function(){
-        if (hr.readyState == 4 && hr.status == 200) {
-            var data = JSON.parse(hr.responseText);
-            results.innerHTML = "";
-            for(var obj in data){
-                results.innerHTML += "<img src=" + data[obj].imageSrc + " name="+data[obj].imageName+">"
-            }
-
-        }
-    }
-    hr.send(null);
-    results.innerHTML = "Requesting...";
 }
-jsonAjax();
+
+function uploadProgress(e) {
+    if (e.lengthComputable) {
+        var percentComplete = Math.round(e.loaded * 100 / e.total);
+        document.getElementById("progress").innerHTML = percentComplete.toString() + '%';
+    } else {
+        document.getElementById("progress").innerHTML = "unable to compute";
+    }
+}
+
+function uploadComplete (e) {
+    alert(e.target.responseText);
+}
+
+function uploadError (e) {
+    alert("Error");
+}
+
+function uploadAbort (e) {
+    alert("Abort");
+}
